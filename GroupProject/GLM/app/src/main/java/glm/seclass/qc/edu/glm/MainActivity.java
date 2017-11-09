5b3e6b2d15f69249ec611ac51b56024e2e2ebec1
Lin <linochoa11690@gmail.com>
2017-11-09 15:50:45 -0500

    fixed errors for creating db

--- GroupProject/GLM/app/src/main/java/glm/seclass/qc/edu/glm/MainActivity.java
+++ GroupProject/GLM/app/src/main/java/glm/seclass/qc/edu/glm/MainActivity.java
@@ -1,9 +1,11 @@
 package glm.seclass.qc.edu.glm;
 
+import android.arch.persistence.room.Room;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
+import glm.seclass.qc.edu.glm.AppDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        AppDatabase database = null;
+        database = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "AppDatabase").build();
     }
 }
 
