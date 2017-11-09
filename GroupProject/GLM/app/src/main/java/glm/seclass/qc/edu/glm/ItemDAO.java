cc45daa50035724c5098a0c17fd431d4d58a9b57
don liu <don@dons-MacBook-Pro.local>
2017-11-09 17:08:02 -0500

    Fixed some errors

---
+++ GroupProject/GLM/app/src/main/java/glm/seclass/qc/edu/glm/ItemDAO.java
@@ -0,0 +1,9 @@
+package glm.seclass.qc.edu.glm;
+
+/**
+ * Created by don on 11/9/17.
+ */
+
+public interface ItemDAO {
+    
+}
--- GroupProject/GLM/app/src/main/java/glm/seclass/qc/edu/glm/groceryListsTable.java
+++ GroupProject/GLM/app/src/main/java/glm/seclass/qc/edu/glm/groceryListsTable.java
@@ -6,7 +6,7 @@ package glm.seclass.qc.edu.glm;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 import android.arch.persistence.room.ColumnInfo;
-
+@Entity
 public class groceryListsTable {
 
 
