0975d80ac8339378313d23f0ceb2bad0ff0b9018
Lin <linochoa11690@gmail.com>
2017-11-13 00:09:28 -0500

    working on UI components

--- GroupProject/GLM/app/src/main/java/glm/seclass/qc/edu/glm/MainActivity.java
+++ GroupProject/GLM/app/src/main/java/glm/seclass/qc/edu/glm/MainActivity.java
@@ -5,11 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+//    final Button createList = findViewById(R.id.createList);
+//    final Button browse = findViewById(R.id.browse);
+//    final Context context = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         GLDatabase db = GLDatabase.getAppDatabase(this);
+        
+//        This commented part is how to create new item
+//        creating tuples for grocery list should be similar to this
 
 //        Item newitem = new Item();
 //        newitem.setItemName("banana");
@@ -22,6 +29,26 @@ public class MainActivity extends AppCompatActivity {
 //            Log.e("items number " + i, example.get(i));
 //        }
 
+
+//      this part is supposed to prompt EditText field
+
+//        createList.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                LayoutInflater layoutInflater = LayoutInflater.from(context);
+//                View promptsView = layoutInflater.inflate(R.layout.activity_main, null);
+//                AlertDialog.Builder alert = new AlertDialog.Builder(context);
+//                alert.setView(promptsView);
+//                final EditText userInput = (EditText) promptsView.findViewById(R.id.newListName);
+//                alert.setCancelable(false).setPositiveButton("Create",
+//                        new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialog, int which) {
+//
+//                            }
+//                        }).setNegativeButton("Cancel", )
+//            }
+//        });
     }
 }
 
