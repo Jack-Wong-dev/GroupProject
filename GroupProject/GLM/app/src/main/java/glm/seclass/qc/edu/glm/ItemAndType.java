113d7e32c9316e7e44e93dc6d8f65e3d19ac3203
don liu <don@dons-MacBook-Pro.local>
2017-11-17 14:48:27 -0500

    Added itemandtype dao, and made item and type an entity

--- GroupProject/GLM/app/src/main/java/glm/seclass/qc/edu/glm/ItemAndType.java
+++ GroupProject/GLM/app/src/main/java/glm/seclass/qc/edu/glm/ItemAndType.java
@@ -1,14 +1,15 @@
 package glm.seclass.qc.edu.glm;
 
 import android.arch.persistence.room.Embedded;
+import android.arch.persistence.room.Entity;
 
 /**
  * Created by Lin on 11/15/17.
  */
-
+@Entity
 public class ItemAndType {
-    @Embedded
+    @Embedded(prefix = "item_")
     Item item;
-    @Embedded
+    @Embedded(prefix = "item_type_")
     ItemType itemType;
 }
